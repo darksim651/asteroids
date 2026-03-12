8739ec83a03373986b44e3a4f0e33f96145ee416
DarkSim651 <darksimmusic@gmail.com>
2026-03-12 20:08:50 +0000

    Working player movement

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS, 0)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        
         pygame.display.flip()
+                                                        # Pauses game loop until 1/60th of a second has passed, for 60fps target
+        dt = my_clock.tick(60) / 1000                   # Convert to ms
+
 
 
 if __name__ == "__main__":
